3bfa65d48b6aa882ee60155ea81453c01525e152
Pranoy <pranoy.mbstu12001@gmail.com>
2021-04-17 23:18:34 +0600

    initialize project

---
+++ src/main/java/com/pranoyit/newtonrest/NewtonRestApplication.java
@@ -0,0 +1,13 @@
+package com.pranoyit.newtonrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtonRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtonRestApplication.class, args);
+	}
+
+}
